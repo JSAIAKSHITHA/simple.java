0c07222c2c7bf30adbf622b9f810d42b378af2b8
JSAIAKSHITHA <jayanthisaiakshitha@gmail.com>
2026-01-16 20:00:49 +0530

    INPUT FILE

---
+++ Task2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.printf("Enter two integers:%n");
+        int m=s.nextInt();
+        int n=s.nextInt();
+          
+        int sum=m+n;
+      
+        System.out.printf("sum:%d%n", sum);
+    }
+}
